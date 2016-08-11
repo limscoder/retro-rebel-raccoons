d013b049a1c44bd22742f2d5ddf5224c5bc79ff2
ZeldaMaster189 <max@maxwellbarber.com>
2016-08-11 16:55:51 -0600

    Added text to video and bio, added picture to bio

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 var videos = [
 {
 	url: 'https://www.youtube.com/embed/PKDodoDF9Qg',
-	title: 'Granny Old Wound'
+	title: 'Tom Hagerman - Granny Old Wound'
 },{
 	url: 'https://www.youtube.com/embed/SpPkih-GXF8',
-	title: "Invisable children's Music"
+	title: "DeVotchKa - Comrade Z"
 },{
 	url: 'https://www.youtube.com/embed/jPMy01lS5as',
-	title: 'Amos Lee - Windows'
+	title: 'Amos Lee - Windows are Rolled Down'
 }
 ];
 
 var selectedVideo = 0;
 function nextVideo() {
-	console.log('next video clicked');
 	selectedVideo = selectedVideo + 1;
 	if (selectedVideo > videos.length - 1) {
 		selectedVideo = 0;
@@ -21,4 +20,18 @@ function nextVideo() {
 
 	var videoEl = window.document.querySelector('.youtube-video');
 	videoEl.setAttribute('src', videos[selectedVideo].url);
+	var titleEl = window.document.querySelector('.video-title');
+	titleEl.innerHTML = videos[selectedVideo].title;
+}
+
+function previousVideo() {
+	selectedVideo = selectedVideo - 1;
+	if (selectedVideo < 0) {
+		selectedVideo = videos.length - 1;
+	}
+
+	var videoEl = window.document.querySelector('.youtube-video');
+	videoEl.setAttribute('src', videos[selectedVideo].url);
+	var titleEl = window.document.querySelector('.video-title');
+	titleEl.innerHTML = videos[selectedVideo].title;
 }
